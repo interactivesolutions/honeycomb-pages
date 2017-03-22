1e89027cc743312cb6dd401cc7b0c952786fc9b7
Liutauras Kavaliauskas <liutauraskav@gmail.com>
2017-03-22 11:38:25 +0200

    Auto generated HCPages package

---
+++ src/app/providers/HCPagesServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace interactivesolutions\honeycombpages\app\providers;
+
+use interactivesolutions\honeycombcore\providers\HCBaseServiceProvider;
+
+class HCPagesServiceProvider extends HCBaseServiceProvider
+{
+    protected $homeDirectory = __DIR__;
+
+    protected $commands = [];
+
+    protected $namespace = 'interactivesolutions\honeycombpages\app\http\controllers';
+
+    public $serviceProviderNameSpace = '{serviceProviderNameSpace}';
+}
+
+
+
+
+
