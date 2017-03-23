72fbe23de7db360c29e1c05ea072751f70c7bfc8
Liutauras Kavaliauskas <liutauraskav@gmail.com>
2017-03-23 14:21:05 +0200

    Added helper function

--- src/app/http/helpers.php
+++ src/app/http/helpers.php
@@ -1,1 +1,25 @@
 <?php
+
+if (!function_exists('makeEmptyNullable'))
+{
+    /**
+     * Updates fields values that are empty strings to null
+     *
+     * @param array $array
+     * @return array
+     */
+    function makeEmptyNullable(array $array)
+    {
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                $value = makeEmptyNullable($value);
+            }
+            if ($value == "")
+                $value = null;
+
+            $array[$key] = $value;
+        }
+
+        return $array;
+    }
+}
