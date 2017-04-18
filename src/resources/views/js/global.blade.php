abe527e4664132dad4d28539c7db9589c4c4e405
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-04-18 22:48:32 +0300

    Revert "Resources views"
    This reverts commit bf75023195f83adc4bc2587758209c1b9b51ccce.

--- src/resources/views/js/global.blade.php
+++
@@ -1,9 +0,0 @@
-{!!
-
-    Minify::javascript([
-        'https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js',
-        'https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js'
-
-    ])
-
-!!}
