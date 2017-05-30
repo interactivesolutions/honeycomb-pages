f9a6f978ad65fa7173b0924d24dab26a648837cb
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-05-30 09:52:14 +0300

    Updated translation keys

--- src/app/forms/HCCategoriesForm.php
+++ src/app/forms/HCCategoriesForm.php
@@ -23,7 +23,7 @@ class HCCategoriesForm
             'buttons'    => [
                 [
                     "class" => "col-centered",
-                    "label" => trans('HCCoreUI::core.button.submit'),
+                    "label" => trans('HCTranslations::core.buttons.submit'),
                     "type"  => "submit",
                 ],
             ],
