946e9e06de731c7ec4c0f39ecf67789371af880c
Mantas Paulauskas <mantopastas@gmail.com>
2017-08-18 08:33:37 +0300

    updated makeemptynullable function with value trimming

--- src/app/http/helpers.php
+++ src/app/http/helpers.php
@@ -1,21 +1,27 @@
 <?php
 
-if (!function_exists('makeEmptyNullable'))
-{
+if( ! function_exists('makeEmptyNullable') ) {
     /**
      * Updates fields values that are empty strings to null
      *
      * @param array $array
+     * @param bool $trimValue
      * @return array
      */
-    function makeEmptyNullable(array $array)
+    function makeEmptyNullable(array $array, $trimValue = false)
     {
-        foreach ($array as $key => $value) {
-            if (is_array($value)) {
-                $value = makeEmptyNullable($value);
+        foreach ( $array as $key => $value ) {
+            if( is_array($value) ) {
+                $value = makeEmptyNullable($value, $trimValue);
             }
-            if ($value == "")
+
+            if( $trimValue && is_string($value) ) {
+                $value = trim($value);
+            }
+
+            if( $value == "" ) {
                 $value = null;
+            }
 
             $array[$key] = $value;
         }
