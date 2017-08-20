cfe20db0212c089a6be7ee7e06fd624710980589
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-08-20 22:46:08 +0300

    updated translations

--- src/resources/lang/en/pages.php
+++ src/resources/lang/en/pages.php
@@ -3,7 +3,6 @@ return [
     'page_title'     => 'Pages',
     'author_id'      => 'Author',
     'publish_at'     => 'Publish at',
-    'expires_at'     => 'Expires at',
     'cover_photo_id' => 'Cover photo',
     'title'          => 'Title',
     'slug'           => 'Slug',
---
+++ src/resources/lang/lt/categories.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'page_title'     => 'Kategorijos',
+    'parent_id'      => 'Pagrindinis',
+    'cover_photo_id' => 'Viršelio nuotrauka',
+    'title'          => 'Antraštė',
+    'slug'           => 'Slug',
+    'content'        => 'Turinys',
+];
