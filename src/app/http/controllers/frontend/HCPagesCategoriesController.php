35aec35cbe142deb1f05038915e141a6563b066b
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-08-21 11:21:19 +0300

    Added category retrieval logics

--- src/app/http/controllers/frontend/HCPagesCategoriesController.php
+++ src/app/http/controllers/frontend/HCPagesCategoriesController.php
@@ -1,8 +1,12 @@
 <?php namespace interactivesolutions\honeycombpages\app\http\controllers\frontend;
 
+use Carbon\Carbon;
 use interactivesolutions\honeycombcore\http\controllers\HCBaseController;
 use interactivesolutions\honeycombpages\app\models\HCPages;
 use interactivesolutions\honeycombpages\app\models\HCPagesCategories;
+use interactivesolutions\honeycombpages\app\models\HCPagesCategoriesConnections;
+use interactivesolutions\honeycombpages\app\models\HCPagesCategoriesTranslations;
+use interactivesolutions\honeycombpages\app\models\HCPagesTranslations;
 
 class HCPagesCategoriesController extends HCBaseController
 {
@@ -13,17 +17,61 @@ class HCPagesCategoriesController extends HCBaseController
     {
         $list = removeRecordsWithNoTranslation(HCPagesCategories::with('translation')->get()->toArray());
 
-        return hcview('HCPages::categories.list', ['data' => ['list' => $list]]);
+        return hcview('HCPages::category.list', ['data' => ['list' => $list]]);
     }
 
     /**
      * Showing all pages related to categories
+     *
      * @param string $lang
      * @param string $slug
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function showCategoriesPages (string $lang, string $slug)
     {
+        return $this->showData($lang, $slug, 'PAGE');
+    }
+
+    /**
+     * Showing all articles related to categories
+     *
+     * @param string $lang
+     * @param string $slug
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showCategoriesArticles (string $lang, string $slug)
+    {
+        return $this->showData($lang, $slug, 'ARTICLE');
+    }
+
+    /**
+     * @param string $lang
+     * @param string $slug
+     * @param string $type
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    private function showData (string $lang, string $slug, string $type)
+    {
+        $category = HCPagesCategoriesTranslations::where('slug', $slug)
+            ->where('language_code', $lang)
+            ->first();
+
+        if (!$category)
+            abort(404, 'Page not found');
+
+        $r = HCPages::getTableName();
+        $c = HCPagesCategoriesConnections::getTableName();
+
+        $query = HCPages::with(['translation', 'categories', 'author'])->select(HCPages::getFillableFields(true));
+
+        $query->join($c, "$r.id", "=", "$c.page_id")
+            ->where("$c.category_id", $category->record_id)
+            ->where("$r.type", $type)
+            ->where("$r.publish_at", '<', Carbon::now());
+
+        $data = $query->get();
+
         //TODO make request to retrieve list of pages based on category id
-        return hcview('HCPages::categories.articles', ['data' => []]);
+        return hcview('HCPages::category.pages', ['data' => ['list' => $data->toArray()]]);
     }
 }
--- src/app/routes/front-end/01_routes.categories.php
+++ src/app/routes/front-end/01_routes.categories.php
@@ -1,6 +1,11 @@
 <?php
 
-Route::group(['prefix' => '{lang}/pages/categories/', 'middleware' => ['web']], function () {
+Route::group(['prefix' => '{lang}/pages/categories/'], function () {
     Route::get('/', ['uses' => 'frontend\\HCPagesCategoriesController@showCategoriesList']);
     Route::get('{slug}', ['uses' => 'frontend\\HCPagesCategoriesController@showCategoriesPages']);
+});
+
+Route::group(['prefix' => '{lang}/articles/categories/'], function () {
+    Route::get('/', ['uses' => 'frontend\\HCPagesCategoriesController@showCategoriesList']);
+    Route::get('{slug}', ['uses' => 'frontend\\HCPagesCategoriesController@showCategoriesArticles']);
 });
